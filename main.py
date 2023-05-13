fae7e2e1389616d14f15dd920ec3b40353b02c84
charlesxjyang <253113-charlesxjyang@users.noreply.replit.com>
2023-05-13 02:54:21 +0000

    finished civilian nuclear reactor

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
-df = pd.read_table("data/nuclear_reactors.txt",delimiter=',')
-print(df.head())
+df = pd.read_table("data/nuclear_reactors.txt", delimiter=',')
+df = df[(df['Status'] != 'Planned') & (df['Status'] != 'Under commissioning') &
+        (df['Status'] != 'Under construction')]
 
-print(df.columns)
-
-df['Commercial operation'] = pd.to_datetime(df['Commercial operation'],dayfirst=True)
+df['Commercial operation'] = pd.to_datetime(df['Commercial operation'],
+                                            dayfirst=True,
+                                            format='mixed')
 
 print(df['Commercial operation'].dt.strftime('%Y'))
+df['Year'] = df['Commercial operation'].dt.strftime('%Y')
+subset_df = df[['Year','Net capacity (MW)']]
+
+print(subset_df.groupby('Year')['Net capacity (MW)'].sum().reset_index())
+
+
